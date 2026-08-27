79d0401ee464fea96a305805334ae78837d128ed
lokossou <lokossousergio156@gmail.com>
2026-08-27 14:27:47 +0100

    update_projet

--- SarefAcademy/urls.py
+++ SarefAcademy/urls.py
@@ -5,9 +5,9 @@ from SarefAcademy import views
 
 urlpatterns = [
     path('',  views.index, name='index'),
-    path('connexion_admin/',  views.connexion_admin, name='connexion_admin'),
-    path('admin_dashboard/',  views.admin_dashboard, name='admin_dashboard'),
-    path('suspension_delai_candidature/',  views.suspension_delai_candidature, name='suspension_delai_candidature'),
-    path('deconnexion/' , views.deconnexion , name="deconnexion"),
+    path('connexion_admin',  views.connexion_admin, name='connexion_admin'),
+    path('admin_dashboard',  views.admin_dashboard, name='admin_dashboard'),
+    path('suspension_delai_candidature',  views.suspension_delai_candidature, name='suspension_delai_candidature'),
+    path('deconnexion' , views.deconnexion , name="deconnexion"),
     
 ]
